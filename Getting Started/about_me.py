84a2c18807f51e3ae65601022df9a57c48c66181
MellaBellaa <97401271+MellaBellaa@users.noreply.github.com>
2022-06-30 14:21:42 -0400

    getting started added

---
+++ Getting Started/about_me.py
@@ -0,0 +1,18 @@
+full_name = "Shamella Myrick"
+nickname = "Mella"
+age = 21
+python_experience = True
+
+hobbies = ["playing trumpet", "listening to audiobooks and podcasts", "practicing spanish"]
+
+favorites = {
+  "movie": "What Dreams May Come",
+  "food": "pasta",
+  "hobby": hobbies[0]
+}
+
+print(full_name, nickname, age, python_experience, "\n")
+print(hobbies, "\n")
+print(favorites)
+
+all_vars = dict(vars())
